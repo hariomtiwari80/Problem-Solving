ab8f0333f9c7be27e6b4b0b6ca4d762f26ee5f5f
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2025-11-21 21:31:47 +0530

    Update Most_Profit_Assigning_Work.java

--- Binary Search/Most_Profit_Assigning_Work.java
+++ Binary Search/Most_Profit_Assigning_Work.java
@@ -19,3 +19,62 @@ class Solution {
 
 -----------------------------------------------------------------------------------
 
+class Solution {
+    class pair{
+        int d,p;
+        pair(int d,int p){
+            this.d=d;
+            this.p=p;
+        }
+    }
+    public int maxProfitAssignment(int[] diff, int[] pro, int[] w) {
+        int n=diff.length,m=w.length;
+        pair[] pa=new pair[n];
+        for(int i=0;i<n;i++){
+            pa[i]=new pair(diff[i],pro[i]);
+        }
+        Arrays.sort(pa,(a,b)->a.d-b.d);
+        Arrays.sort(w);
+        int ans=0;
+        for(int x:w){
+            int max=0;
+            for(int i=0;i<n;i++){
+                if(pa[i].d>x) break;
+                max=Math.max(max,pa[i].p);
+            }
+            ans+=max;
+        }
+        return ans;
+    }
+}
+
+------------------------------------------------------------------------
+
+class Solution {
+    class pair{
+        int d,p;
+        pair(int d,int p){
+            this.d=d;
+            this.p=p;
+        }
+    }
+    public int maxProfitAssignment(int[] diff, int[] pro, int[] w) {
+        int n=diff.length,m=w.length;
+        pair[] pa=new pair[n];
+        for(int i=0;i<n;i++){
+            pa[i]=new pair(diff[i],pro[i]);
+        }
+        Arrays.sort(pa,(a,b)->a.d-b.d);
+        Arrays.sort(w);
+        int ans=0,j=0, max=0;
+        for(int x:w){
+            while(j<n && pa[j].d<=x){
+                max=Math.max(max,pa[j].p);
+                j++;
+            }
+            ans+=max;
+        }
+        return ans;
+    }
+}
+
