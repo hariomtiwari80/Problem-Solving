b07b21ab3ecb960821ce8b4e8a46410552f123b0
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2025-11-24 00:02:30 +0530

    Update Most_Beautiful_Item_for_Each_Query.java

--- Binary Search/Most_Beautiful_Item_for_Each_Query.java
+++ Binary Search/Most_Beautiful_Item_for_Each_Query.java
@@ -44,3 +44,32 @@ class Solution {
         return ans;
     }
 }
+
+----------------------------------------------------------------------
+
+class Solution {
+    public int[] maximumBeauty(int[][] item, int[] q) {
+        int n=q.length;
+        Arrays.sort(item,(a,b)->a[0]-b[0]);
+        int max=item[0][1];
+        for(int i=1;i<item.length;i++){
+            max=Math.max(max,item[i][1]);
+            item[i][1]=max;
+        }
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=BinarySearch(item,q[i]);
+        }
+        return ans;
+    }
+    public int BinarySearch(int[][] item,int n){
+        int l=0,h=item.length-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(item[m][0]>n) h=m-1;
+            else l=m+1;
+        }
+        if (h<0) return 0;
+        return item[h][1];
+    }
+}
