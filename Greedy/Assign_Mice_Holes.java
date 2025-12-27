99e9aec03e72218be1bd314408d2b4d2eb1fdf0e
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2025-12-27 12:19:37 +0530

    Create Assign_Mice_Holes.java

---
+++ Greedy/Assign_Mice_Holes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int assignHole(int[] mices, int[] holes) {
+        int n=mices.length;
+        Arrays.sort(mices);
+        Arrays.sort(holes);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(holes[i]>mices[i]) ans=Math.max(ans,holes[i]-mices[i]);
+            else ans=Math.max(ans,mices[i]-holes[i]);
+        }
+        return ans;
+        
+    }
+};
