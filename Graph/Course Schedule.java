ab0021aa020fe77157a5a72499fdde015494805e
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-04-04 17:50:47 +0530

    Create Course Schedule.java

---
+++ Graph/Course Schedule.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canFinish(int n, int[][] arr) {
+        int[] in=new int[n];
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<arr.length;i++){
+            adj.get(arr[i][1]).add(arr[i][0]);
+            in[arr[i][0]]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(in[i]==0){
+                q.add(i);
+                c++;
+            }
+        }
+        while(!q.isEmpty()){
+            int t=q.poll();
+            for(int i:adj.get(t)){
+                in[i]--;
+                if(in[i]==0){
+                    c++;
+                    q.add(i);
+                }
+            }
+        }
+        return c==n;
+    }
+}
