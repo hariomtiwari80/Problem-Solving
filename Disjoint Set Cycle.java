2fb06be9634ac483aea583af49606c79a32a8da2
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-04-09 11:24:10 +0530

    Create Disjoint Set Cycle.java

---
+++ Disjoint Set Cycle.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] parent;
+    public int[] rank;
+    
+    public void union(int i,int j){
+        int p1=find(i);
+        int p2=find(j);
+        if(p1==p2) return;
+        if(rank[p1]>rank[p2]){
+            parent[j]=p1;
+        }
+        else if(rank[p2]>rank[p1]){
+            parent[i]=p2;
+        }
+        else{
+            parent[j]=p1;
+            rank[p1]++;
+        }
+    }
+    
+    public int find(int i){
+        if(i==parent[i]) return i;
+        return parent[i]=find(parent[i]);
+    }
+    
+    public boolean isCycle(int V, int[][] ed) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        parent=new int[V];
+        rank=new int[V];
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<ed.length;i++){
+            adj.get(ed[i][0]).add(ed[i][1]);
+            adj.get(ed[i][1]).add(ed[i][0]);
+        }
+        
+        for(int i=0;i<V;i++){
+            for(int j:adj.get(i)){
+                if(i<j){
+                    int p1=find(i);
+                    int p2=find(j);
+                    if(p1==p2) return true;
+                    union(i,j);
+                }
+            }
+        }
+        return false;
+        
+    }
+}
