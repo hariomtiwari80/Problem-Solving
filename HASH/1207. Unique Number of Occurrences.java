3fcfb996e400ff0946315eb4ccf4f05299ee3850
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-07-01 13:05:39 +0530

    Create 1207. Unique Number of Occurrences.java

---
+++ HASH/1207. Unique Number of Occurrences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:arr) map.put(i,map.getOrDefault(i,0)+1);
+        HashSet<Integer> set=new HashSet<>();
+        for(int k:map.keySet()){
+            if(set.contains(map.get(k))) return false;
+            set.add(map.get(k));
+        }
+        return true;
+    }
+}
