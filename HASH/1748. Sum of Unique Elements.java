2ca12907ee865a5da7cca977161fb1d45a2acfcb
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-07-01 12:49:40 +0530

    Create 1748. Sum of Unique Elements.java

---
+++ HASH/1748. Sum of Unique Elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfUnique(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)==1) sum+=key;
+        }
+        return sum;
+    }
+}
