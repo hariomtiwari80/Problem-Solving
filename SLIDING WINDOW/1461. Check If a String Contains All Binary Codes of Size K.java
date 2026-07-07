f0df3a0ceac277a7cc630c3607264fd737908b02
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-07-07 08:57:01 +0530

    Create 1461. Check If a String Contains All Binary Codes of Size K.java

---
+++ SLIDING WINDOW/1461. Check If a String Contains All Binary Codes of Size K.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        HashSet<String> set=new HashSet<>();
+        int i=0,j=0;
+        while(j<s.length()){
+            if(j-i+1==k){
+                set.add(s.substring(i,j+1));
+                i++;
+            }
+            j++;
+        }
+        return set.size()==Math.pow(2,k);
+    }
+}
