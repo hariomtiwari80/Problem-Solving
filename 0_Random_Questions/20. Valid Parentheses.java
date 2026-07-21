615593c421fa5eb6b6e6978774c31377ebba7517
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-07-21 14:43:58 +0530

    Create 20. Valid Parentheses.java

---
+++ 0_Random_Questions/20. Valid Parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(' || c=='[' || c=='{') st.push(c);
+            else{
+                if(st.isEmpty() || ((st.peek()!='(' && c==')') || (st.peek()!='[' && c==']') || (st.peek()!='{' && c=='}'))) return false;
+                st.pop();
+            }
+        }
+        return st.size()==0;
+    }
+}
