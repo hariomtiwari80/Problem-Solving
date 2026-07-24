579754ac5a292a92d3a7e774be220d45641fec06
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-07-24 18:02:13 +0530

    Create Aggressive Cows.java

---
+++ 0_Random_Questions/Aggressive Cows.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int aggressiveCows(int[] arr, int k) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        int l=1,h=arr[n-1];
+        int ans=0;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(valid(arr,m,k)){
+                ans=m;
+                l=m+1;
+            }
+            else h=m-1;
+        }
+        return ans;
+    }
+    public boolean valid(int[] arr,int m,int k){
+        int c=1,last=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-last>=m){
+                c++;
+                last=arr[i];
+            }
+            if(c==k) return true;
+        }
+        return c>=k;
+    }
+}
