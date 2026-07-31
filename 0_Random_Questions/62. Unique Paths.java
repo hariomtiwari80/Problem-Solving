480310f929308f2adcc562fc39718af6ea0f3a6c
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-07-31 11:28:05 +0530

    Create 62. Unique Paths.java

---
+++ 0_Random_Questions/62. Unique Paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp=new int[n];
+        for(int i=0;i<n;i++) dp[i]=1;
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[j]=dp[j]+dp[j+1];
+            }
+        }
+        return dp[0];
+    }
+}
